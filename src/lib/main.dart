2940b35d2c5bb21d80c0bffbf6763fd560e9e64f
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-29 16:24:41 +0000

    Create Subtitle component, Left and Right Column

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -12,95 +12,127 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Flutter CV',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          surface: Color(0xFFF2EEE9),
+        ),
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+            fontFamily: "Cooper Hewitt",
+            letterSpacing: -5,
+          ),
+          titleSmall: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Jesus Lugo CV'),
+      home: Scaffold(body: Body()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Body extends StatelessWidget {
+  const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+    return Container(
+      margin: EdgeInsets.all(40),
+      color: Theme.of(context).colorScheme.surface,
+      child: Center(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          children: [LeftColumn(), SizedBox(width: 20), RightColumn()],
+        ),
+      ),
+    );
+  }
+}
+
+class Subtitle extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color iconBackgroundColor;
+
+  const Subtitle(
+    this.text, {
+    super.key,
+    this.icon = Icons.mic,
+    this.iconBackgroundColor = const Color(0xFF01C9AD),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: BoxBorder.all(color: Colors.black),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(5, 5, 10, 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: iconBackgroundColor,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Icon(icon),
             ),
+            SizedBox(width: 10),
+            Text(text, style: Theme.of(context).textTheme.titleMedium),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class LeftColumn extends StatelessWidget {
+  const LeftColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [Header()]);
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "MARIANA\nNAPOLITANI",
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+        SizedBox(height: 30),
+        Subtitle("DIGITAL MARKETING"),
+      ],
+    );
+  }
+}
+
+class RightColumn extends StatelessWidget {
+  const RightColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [SizedBox(height: 50), PersonPicture()]);
+  }
+}
+
+class PersonPicture extends StatelessWidget {
+  const PersonPicture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: 235, height: 300, color: Color(0xFFAB2733));
   }
 }
